ddcfe95400e22a950d8d49e09c356aad33bfb5e1
STBxD <87096507+STBxD@users.noreply.github.com>
2021-09-07 01:49:08 +0530

    Update karma.py

--- KURUMIBOT/modules/karma.py
+++ KURUMIBOT/modules/karma.py
@@ -1,5 +1,4 @@
 
-from KURUMIBOT.utils.errors import capture_err
 from KURUMIBOT.modules.mongo.karma_mongo import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
 from KURUMI.utils.filter_groups import karma_positive_group, karma_negative_group
