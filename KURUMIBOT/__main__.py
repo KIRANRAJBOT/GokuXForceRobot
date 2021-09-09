4c9756a2a96b533e6d1e7af8e59caf47832bbe60
STBxD <87096507+STBxD@users.noreply.github.com>
2021-09-10 01:32:15 +0530

    Update __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -18,6 +18,7 @@ from KURUMIBOT import (
     dispatcher,
     StartTime,
     telethn,
+    pbot,
     updater)
 
 # needed to dynamically load modules
@@ -722,4 +723,5 @@ def main():
 if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
+    pbot.start()
     main()
