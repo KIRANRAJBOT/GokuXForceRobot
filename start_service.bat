440eb57c22bfb63d562a9af35ebac1e3cdfeb412
STBxD <87096507+STBxD@users.noreply.github.com>
2021-09-12 01:03:42 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -1,6 +1,6 @@
 @echo off
 :: This runs the batch file as an admin - required UAC to be off
-:: This is just an asty hack in to get job done cause we host it on windows dedi.
+:: This is just an asty hack in to get job done caus we host it on windows dedi.
 :: BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
