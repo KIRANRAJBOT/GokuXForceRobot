d8956778a124009a276f6c7649109dd50c546fe9
STBxD <87096507+STBxD@users.noreply.github.com>
2021-09-12 00:49:31 +0530

    Update __main__.py

--- XForceSTB/__main__.py
+++ XForceSTB/__main__.py
@@ -708,7 +708,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Eren is now alive and functioning")
+        LOGGER.info("Goku is now alive and functioning")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
